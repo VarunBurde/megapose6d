Need to fix scaling in scene renderer

src/megapose/panda3d_renderer/panda3d_batch_renderer.py
@@ -378,7 +378,7 @@ class Panda3dBatchRenderer:
             w2c = np.matmul(w2c, world_tranformation)
 
             # convert back to meters
-            w2c[:3, 3] = w2c[:3, 3] / 1000
+            w2c[:3, 3] = w2c[:3, 3] / 350
 
             c2w = np.linalg.inv(w2c)
 

src/megapose/panda3d_renderer/panda3d_scene_renderer.py
@@ -404,12 +404,13 @@ class Panda3dSceneRenderer:
                 Transformation = np.matmul(Transformation, world_tranformation)
 
                 # convert back to meters
-                Transformation[:3, 3] = Transformation[:3, 3] / 1000
+                Transformation[:3, 3] = Transformation[:3, 3] / 350
 
+                Transformation = np.linalg.inv(Transformation)
 
                 rotation = Rsci.from_matrix(Transformation[:3, :3])
-                print("Transformation", Transformation[:3, 3])
-                print("rotation", rotation.as_euler("xyz", degrees=True))
+                # print("Transformation", Transformation[:3, 3])
+                # print("rotation", rotation.as_euler("xyz", degrees=True))
                 rgb = ngp_renderer.get_image_from_tranform(Transformation, "Shade")
                 rgb = np.array(rgb, dtype=np.uint8)
 