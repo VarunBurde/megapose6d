68cfd37a7cabb7d414febfca6347bbfbc424457c
karolina <varunburde@gmail.com>
2024-03-20 09:36:43 +0100

    working megapose karolina

---
+++ env.sh
@@ -0,0 +1,11 @@
+
+
+module load CUDA
+module load libglvnd/1.3.3-GCCcore-11.2.0
+module load CMake/3.22.1-GCCcore-11.2.0
+module load libGLU/9.0.2-GCCcore-11.2.0
+module load Mesa/21.1.7-GCCcore-11.2.0
+module load GLib/2.69.1-GCCcore-11.2.0
+module load Python/3.9.6-GCCcore-11.2.0
+
+
